20667e7c0304cdcac2da6dc2f86f2eb3b4059e5d
Andrea Galbusera <gizero@gmail.com>
2017-04-30 18:01:46 +0200

    add class EnterTextSimpleDialog

---
+++ app/src/main/java/android/androidVNC/EnterTextSimpleDialog.java
@@ -0,0 +1,89 @@
+package android.androidVNC;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import java.io.IOException;
+
+class EnterTextSimpleDialog extends Dialog {
+    private EditText _textEnterText;
+
+    private VncCanvasActivity _canvasActivity;
+
+    EnterTextSimpleDialog(Context context){
+        super(context);
+        setOwnerActivity((Activity)context);
+        _canvasActivity = (VncCanvasActivity)context;
+    }
+
+    // should be factored out with same method from EnterTextDialog
+    private void sendText(String s)
+    {
+        RfbProto rfb = _canvasActivity.vncCanvas.rfb;
+        int l = s.length();
+        for (int i = 0; i<l; i++)
+        {
+            char c = s.charAt(i);
+            int meta = 0;
+            int keysym = c;
+            if (Character.isISOControl(c))
+            {
+                if (c=='\n')
+                    keysym = MetaKeyBean.keysByKeyCode.get(KeyEvent.KEYCODE_ENTER).keySym;
+                else
+                    continue;
+            }
+            try
+            {
+                rfb.writeKeyEvent(keysym, meta, true);
+                rfb.writeKeyEvent(keysym, meta, false);
+            }
+            catch (IOException ioe)
+            {
+                // TODO: log this
+            }
+        }
+    }
+
+    /* (non-Javadoc)
+ * @see android.app.Dialog#onCreate(android.os.Bundle)
+ */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.entertextsimple);
+        setTitle(R.string.enter_text_title);
+        _textEnterText = (EditText)findViewById(R.id.textEnterText);
+
+        (findViewById(R.id.buttonSendText)).setOnClickListener(new View.OnClickListener() {
+
+            /* (non-Javadoc)
+             * @see android.view.View.OnClickListener#onClick(android.view.View)
+             */
+            @Override
+            public void onClick(View v) {
+                sendText(_textEnterText.getText().toString());
+                _textEnterText.setText("");
+                dismiss();
+            }
+        });
+    }
+
+    /* (non-Javadoc)
+     * @see android.app.Dialog#onStart()
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        _textEnterText.requestFocus();
+        InputMethodManager imm = (InputMethodManager) _canvasActivity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+    }
+
+}
